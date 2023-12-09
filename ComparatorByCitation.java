57a07e84184a2c5ff1bbefaaccfed3c684ee8fd8
Amir Adilov <112765709+n31t@users.noreply.github.com>
2023-12-09 23:12:00 +0600

    Add files via upload
    a

---
+++ ComparatorByCitation.java
@@ -0,0 +1,18 @@
+package researchWorks;
+
+
+/**
+* @generated
+*/
+public class ComparatorByCitation extends ResearchPaperComparator {
+    
+    
+    
+
+    
+    
+
+    //                          Operations                                  
+    
+    
+}
